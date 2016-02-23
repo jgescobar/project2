9de0737fcafeee4f91e6a777328259ce2f88488a
Jasmine Guzman-Escobar <jasmineguzmanescobar@yahoo.com>
2016-02-22 19:56:03 -0800

    sign up and login in created

--- through_the_lens_app/config/routes.rb
+++ through_the_lens_app/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  resources :users
+  resources :users, only: [:new, :create, :show]
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  root 'users#show'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +62,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
