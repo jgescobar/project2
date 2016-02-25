dd7d00a203acacbc0c7f2d94c31a3f7443612bcf
Jasmine Guzman-Escobar <jasmineguzmanescobar@yahoo.com>
2016-02-24 20:22:38 -0800

    pretty buttons

--- through_the_lens_app/app/controllers/shares_controller.rb
+++ through_the_lens_app/app/controllers/shares_controller.rb
@@ -11,12 +11,15 @@ class SharesController < ApplicationController
   end
 
   def create
-    share = Share.new(params[:share])
+    share = Share.new(share_params)
     share.user = current_user
     if share.save
       redirect_to '/shares?category=' + share.category
     end
-
   end
 
+  private
+  def share_params
+    params.require(:share).permit(:link, :title, :description, :category, :artist, :length, :address, :album)
+  end
 end
